3685efd9ab0a269be8acafc65d12fd78bd156024
andalzo <esancak.hesare@gmail.com>
2023-07-26 23:36:03 +0300

    Removed asio, instead started using sockets. THERE ARE MANY PROBLEMS ARISED, BUT EVENTUALLY IT WAS BUILT :D

---
+++ src/implementation/message_handler/net/GenericSocket.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "common/mavlink.h"
+#include "infrastructure/TSQueue.h"
+#include <iostream>
+#include <thread>
+#include <atomic>
+
+namespace FW
+{
+    class GenericSocket : public std::enable_shared_from_this<GenericSocket>
+    {
+    public:
+        GenericSocket() = default;
+        virtual bool Start() = 0; //This name convention for UDP is just triggering the reading
+        virtual void Close() = 0;
+
+        void Send(const mavlink_message_t& msg);
+        bool IsOpen() const;
+        TSQueue<mavlink_message_t>& Incoming();
+        virtual ~GenericSocket() = default;
+        virtual void WriterThread() = 0;
+        virtual void ReaderThread() = 0;
+    protected:
+
+        TSQueue<mavlink_message_t> out_queue;
+        TSQueue<mavlink_message_t> incoming_queue;
+        std::atomic<bool> is_open = false;
+    };
+} // FW
+
+
